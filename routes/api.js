23c00029d97e1d33226f91c10c950d336960247b
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-09 00:14:52 -0500

    another get route

--- routes/api.js
+++ routes/api.js
@@ -11,3 +11,13 @@ router.get("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
