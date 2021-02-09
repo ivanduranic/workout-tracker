e85647304f3d700f300f33ef49926f26882af35d
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-09 00:16:26 -0500

    put route

--- routes/api.js
+++ routes/api.js
@@ -31,3 +31,19 @@ router.post("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
