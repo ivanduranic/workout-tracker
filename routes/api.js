6422d4f8751374735c7d2176d8bca27e81501c33
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-09 00:13:44 -0500

    get route

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
