ab0ccdd8aca1a80f245d5b153d85c3129347abc4
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-09 00:09:51 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+const MONGODB_URI =
+  "mongodb+srv://ivanduranic:Wanderlei@1@cluster0.gljok.mongodb.net/Workout?retryWrites=true&w=majority";
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-app.post("/submit", ({ body }, res) => {
-  User.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
