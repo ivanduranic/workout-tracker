ad7388d7229f7f5e80117483ea571f21570e4d9e
Ivan Duranic <61889668+ivanduranic@users.noreply.github.com>
2021-02-16 21:18:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const MONGODB_URI =
-  "mongodb+srv://ivanduranic:Wanderlei@1@cluster0.gljok.mongodb.net/Workout?retryWrites=true&w=majority";
+  "mongodb+srv://ivanduranic:<password>@cluster0.gljok.mongodb.net/Workout?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
